9d85fc96d4b7d6ec51e81e21734fe7009f10fa1e
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-01-04 20:58:35 -0300

    udemy

--- Dia-1/index.js
+++ Dia-1/index.js
@@ -1,5 +1,8 @@
-console.log('JavaScript funcionado')
 
+let itemName = 'Pen' //variavel tipo string
+let itemPrice = 3 // variavel tipo numero-number
+let itemAvailable = true // variavel tipo boolean
+let itemColor   // variavel tipo UNDEFINIED
+itemName = null   //apaga o valor da variável
 
-let name = 'Walace'
-console.log(name)
+console.log(itemName)
