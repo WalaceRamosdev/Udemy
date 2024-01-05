db4975cd204de768ead6d3e606d10ab2ef908ef3
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-01-05 20:45:48 -0300

    udemy

--- Dia-1/index.js
+++ Dia-1/index.js
@@ -2,10 +2,22 @@
 let itemName = 'Pen' //variavel tipo string
 let itemPrice = 3 // variavel tipo numero-number
 let itemAvailable = true // variavel tipo boolean
-let itemColor   // variavel tipo UNDEFINIED
+let itemColor = 'Red'  // variavel tipo UNDEFINIED
 itemName = null   //apaga o valor da variável
 
 
 //variáveis de referencia 
 
 // objects, arrays e functions
+
+// criando objetos em java script
+
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+
+}
+
+console.log(pen)
