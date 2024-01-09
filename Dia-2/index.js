e67da3f2cf363b0180c8f98c2184423cb4791de6
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-01-09 20:59:05 -0300

    Create index.js

---
+++ Dia-2/index.js
@@ -0,0 +1,10 @@
+let num1 = 3
+let num2 = 2
+let total = num1 + num2
+
+console.log(total++)
+console.log(total)
+
+// ++ estes sinais juntos acrescentam semprem +1 ao total, e pode ser usado anes ou depois do mesmo
+
+// -- estes sinais juntos diminuem semprem -1 ao total, e pode ser usado anes ou depois do mesmo
