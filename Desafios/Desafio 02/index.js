c72e891fa4adb3336dc6e9e6fc57ab27b9d5a42b
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-02-15 08:42:43 -0300

    Update index.js

--- Desafios/Desafio 02/index.js
+++ Desafios/Desafio 02/index.js
@@ -1,2 +1,8 @@
 let productValue = Number(prompt('Entre com o valor do produto: '))
 console.log(productValue)
+
+if (productValue >= 20){
+    document.getElementById('result').innerHTML = 'Aprovado'
+} else {
+    document.getElementById('result').innerHTML = 'Negado'
+}
