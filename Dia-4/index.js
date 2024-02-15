e4356d52e924ab9fe53acb19ffbdc35cba7a6167
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-02-15 11:49:02 -0300

    array

--- Dia-4/index.js
+++ Dia-4/index.js
@@ -1,7 +1,12 @@
-const movies = [
-    {id: 1, movieName: 'Gigantes de aço'},
-    {id: 2, movieName: 'Spirit'},
-    {id: 3, movieName: 'El Dorado'}
-]
+//const movies = [
+  //  {id: 1, movieName: 'Gigantes de aço'},
+    //{id: 2, movieName: 'Spirit'},
+    //{id: 3, movieName: 'El Dorado'}
+//]
 
-console.log(movies.find(movie => movie.movieName == 'Spirit'))
+//console.log(movies.find(movie => movie.movieName == 'Spirit'))
+
+//removendo itens da array
+
+const num = [5, 6, 7, 8]
+console.log(num)
