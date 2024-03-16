218bc104d64de98a4098b1aa9117ecb151ed6e0c
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 23:32:04 -0300

    end

--- Dia-2/whileloop.js
+++ Dia-2/whileloop.js
@@ -1,5 +1,11 @@
+/*
 
+    Dentro do forloop, a variável fica dentro da condição, no whileloop, a variável fica fora da condição
+
+*/
 
 for (i = 10; i < 21; i++) {
     console.log('Número', i)
 }
+
+
