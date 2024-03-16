5a5042a3d5242ba9558d8850ca0db1e74efdf5a9
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 22:47:23 -0300

    looping

--- Dia-2/forloop.js
+++ Dia-2/forloop.js
@@ -1,3 +1,11 @@
+/*
+    Para iniciarmos um looping, temos que ter noção de que ele é dividido em 3 partes.
+    Declaramos a sua variável, sua condição e seu incremento.
+    Por padrão, uma variável dentro de um looping é chamada de i.
+    Na condição, devemos por até quando a o looping deve trabalhar.
+    No incremento devemos dizer quanto deve ser acrescentado a variavel até chegar ao valor desejado.
+*/
+
 for (i = 10; i < 21; i++) {
     console.log('Número', i)
 }
