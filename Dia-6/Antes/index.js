f31aa9bf759ccf815fed61e7b95570a71f78f4be
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-03 19:34:37 -0300

    atribuindo o nome das cores para a página

--- Dia-6/Antes/index.js
+++ Dia-6/Antes/index.js
@@ -2,17 +2,19 @@
 
 const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
 const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
 
 /* Aqui será criado as funções*/
 
 btn.addEventListener('click', function(){
-    document.body.style.background = colors[3]
     const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
     //console.log(randomColor)
 })
 
 /* Criando uma função para numero randomico*/
 
 function getRandomColor () {
-    return Math.floor(Math.random() * colors.lenght)
+    return Math.floor(Math.random() * colors.length)
 }
