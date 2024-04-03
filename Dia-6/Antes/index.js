d4b4d80f00a09d764bdd88f9a36ff9514db73472
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-03 19:17:32 -0300

    adicionando event listener

--- Dia-6/Antes/index.js
+++ Dia-6/Antes/index.js
@@ -1,3 +1,10 @@
+/*Iremos aqui criar as variaveis*/
 
 const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
 const btn = document.getElementById('btn')
+
+/* Aqui será criado as funções*/
+
+btn.addEventListener('click', function(){
+    document.body.style.background = colors[1]
+})
