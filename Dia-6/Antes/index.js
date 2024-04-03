fae488c09bb34813269fbcc523af4547ddf81106
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-03 19:26:03 -0300

    criando cores randomicas

--- Dia-6/Antes/index.js
+++ Dia-6/Antes/index.js
@@ -6,5 +6,13 @@ const btn = document.getElementById('btn')
 /* Aqui será criado as funções*/
 
 btn.addEventListener('click', function(){
-    document.body.style.background = colors[1]
+    document.body.style.background = colors[3]
+    const randomColor = getRandomColor()
+    //console.log(randomColor)
 })
+
+/* Criando uma função para numero randomico*/
+
+function getRandomColor () {
+    return Math.floor(Math.random() * colors.lenght)
+}
