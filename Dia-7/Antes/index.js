5ec62c1557d6ec96658e9bef9623d4ed95982319
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-04 10:12:45 -0300

    projeto finalizado

--- Dia-7/Antes/index.js
+++ Dia-7/Antes/index.js
@@ -2,6 +2,12 @@
 
 let computerNumber
 let userNumbers = []
+let attempts = 0
+let maxGuesses = 10
+
+function newGame () {
+    window.location.reload()
+}
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
@@ -16,15 +22,29 @@ function compareNumbers() {
 
     /* Adicionando a parte lógica para a página */
 
-    if (userNumber > computerNumber) {
-        document.getElementById ('textOutput').innerHTML = 'Seu número é muito alto'
+    if ( attempts < maxGuesses) {
 
-    } else if (userNumber < compareNumbers) {
-        document.getElementById ('textOutput').innerHTML = 'Seu número é muito baixo'
-
+        if (userNumber > computerNumber) {
+            document.getElementById ('textOutput').innerHTML = 'Seu número é muito alto'
+            document.getElementById ('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+    
+        } else if (userNumber < compareNumbers) {
+            document.getElementById ('textOutput').innerHTML = 'Seu número é muito baixo'
+            document.getElementById ('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        } else {
+            document.getElementById ('textOutput').innerHTML = 'Parabéns, você acertou !'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+        }
+    
     } else {
-        document.getElementById ('textOutput').innerHTML = 'Parabéns, você acertou !'
-        
+        document.getElementById('textOutput').innerHTML = 'Você Perdeu ! O número do computador é ' + computerNumber
+        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
     }
 
 }
