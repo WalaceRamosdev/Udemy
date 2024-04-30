8b4b6dcfba26f68cd6f68a30dadc21f9c0042dfd
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-30 20:42:01 -0300

    primitivos

---
+++ Dia-2/primitivos.js
@@ -0,0 +1,28 @@
+/* Existem dois tipos de variáveis...
+as variaveis primitivas e as referenciais ou de referencia
+
+Vamos falar sobre as variaveis primitivas = 
+
+
+String - Este tipo recebe valores no formato de texto
+
+Number - Recebe valores no formato de números
+
+Boolean - Recebe apenas dois valores = True ou False
+
+Undefinied - Esta é uma variável que não teve seu valor ainda definido
+
+Null - É utilizado para apagar o valor de determinada variavel 
+
+=> (itemName = 'Pen') (itemName = null => o valor que antes era Pen, agora está vazio na variavel)
+
+Ex.:
+
+*/
+
+let itemName = 'Pen'
+let itemPrice = 3
+let itemAvailable = true
+let itemColor
+
+
