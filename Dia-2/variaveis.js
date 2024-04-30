cd8f2f883cf1fe0430a98ac0096cc9c3dc1d1c93
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-30 20:15:11 -0300

    variaveis

---
+++ Dia-2/variaveis.js
@@ -0,0 +1,11 @@
+/* Criando a primeira variável...
+Elas são geralmente iniciadas por let ou const.
+As variáveis são usadas para se armazenar informações ou dados */
+
+/* É sempre interessante se utilizar os nomes das variáveis com dois nomes...sempre iniciando em letra minuscula e o segundo nome se iniciando em letra maiuscula, conhecido como camelcase, também não se pode começar com números, não pode conter espaços e por ultimo, letras maiusculas e minusculas importam para o javacript. Sempre ter atenção nas nomenclaturas das variaveis */
+
+let eyesColor = 'blue'
+let hairColor = 'Black' 
+console.log (hairColor)
+
+
