4d860c1ba8ac402bd7d2c61c9ce6faf91818b79a
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-30 20:30:25 -0300

    asi

---
+++ Dia-2/asi.js
@@ -0,0 +1,5 @@
+/* Definindo a utilização do ponto e vírgula...
+
+O JavaScript é uma linguagem que não obriga aos seus usuários estarem finalizando suas linhas de código com ;
+Esta prática não influencia na leitura ou no processamento do código. 
+O código irá funcionar com a utilização ou não do mesmo */
